a935726c9a5dd3c02dad637e6cffd870c6be2897
Munir Shyabal <munir.shyabal@hotmail.com>
2022-01-31 19:21:29 +0000

    Added vpc module fifth time

--- main.tf
+++ main.tf
@@ -15,7 +15,13 @@ provider "google" {
 module "network" {
   source  = "app.terraform.io/SnoopyToMars/network/google"
   version = "3.4.0"
-  # insert required variables here
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
 
 resource "google_compute_network" "hashicat" {
