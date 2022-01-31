349c82f835dd30f1ee6ecda090dd687ce2a9d3b8
Munir Shyabal <munir.shyabal@hotmail.com>
2022-01-31 19:37:20 +0000

    Added vpc module seventh time

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,14 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-
-    subnets = [
-        {
-            subnet_name   = "gaurav-subnet"
-            subnet_ip     = "10.100.10.0/24"
-            subnet_region = var.region
-        },
-    ]
+module "network" {
+  source  = "app.terraform.io/SnoopyToMars/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  network_name = "gaurav-network"
+  project_id   = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
